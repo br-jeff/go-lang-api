b3cf0f5187e9ae6589d69dedee895b3faa432b46
Jefferson Silva <jefferson_1996Th@hotmail.com>
2024-01-07 01:19:37 -0300

    fix logger f to lowercase

--- config/logger.go
+++ config/logger.go
@@ -41,18 +41,18 @@ func (l *Logger) Error(v ...interface{}) {
 	l.debug.Println(v...)
 }
 
-func (l *Logger) DebugF(format string, v ...interface{}) {
+func (l *Logger) Debugf(format string, v ...interface{}) {
 	l.debug.Printf(format, v...)
 }
 
-func (l *Logger) InfoF(format string, v ...interface{}) {
+func (l *Logger) Infof(format string, v ...interface{}) {
 	l.debug.Printf(format, v...)
 }
 
-func (l *Logger) WarningF(format string, v ...interface{}) {
+func (l *Logger) Warningf(format string, v ...interface{}) {
 	l.debug.Printf(format, v...)
 }
 
-func (l *Logger) ErrorF(format string, v ...interface{}) {
+func (l *Logger) Errorf(format string, v ...interface{}) {
 	l.debug.Printf(format, v...)
 }
