01811f51e9f29b423ad0f52e2a6f9b26c033125b
Jefferson Silva <jefferson_1996Th@hotmail.com>
2024-02-21 14:11:26 -0300

    add viper and start configurations

---
+++ config/viper.go
@@ -0,0 +1,21 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+func NewViper() *viper.Viper {
+	settings := viper.New()
+
+	settings.SetConfigName("config")
+	settings.SetConfigType("json")
+	settings.AddConfigPath("./")
+
+	err := settings.ReadInConfig()
+
+	if err != nil {
+		panic(fmt.Errorf("viper not loading error %w \n", err))
+	}
+}
